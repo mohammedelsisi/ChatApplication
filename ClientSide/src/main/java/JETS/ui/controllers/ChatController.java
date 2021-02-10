cb22afb1e38a56f29e21633770b7f41c5a92b8be
dalia.elzohairy.de@gmail.com <dalia.elzohairy.de@gmail.com>
2021-02-10 23:38:07 +0200

    upperSideBarIds

--- ClientSide/src/main/java/JETS/ui/controllers/ChatController.java
+++ ClientSide/src/main/java/JETS/ui/controllers/ChatController.java
@@ -1,8 +1,25 @@
 package JETS.ui.controllers;
 
+import javafx.scene.Group;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class ChatController {
+    public Group AddFriend;
+
     public void switchToSecondary(KeyEvent keyEvent) {
+    }
+
+    public void requestFriend(MouseEvent mouseEvent){
+        Alert a1 = new Alert(Alert.AlertType.NONE,
+                "Here is the info about Notepad", ButtonType.OK);
+
+        a1.setGraphic(new ImageView(new Image("download.png")));
+        a1.setTitle("About Us");
+        a1.show();
     }
 }
